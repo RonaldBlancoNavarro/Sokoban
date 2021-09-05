1580d3e5e365f1e2416aa0534d9678f713f3bae8
Roanld Blanco Navarro <ronald.blanco.navarro@est.una.ac.cr>
2021-09-04 23:22:51 -0600

    Menus

--- Sokoban/Juego.cpp
+++ Sokoban/Juego.cpp
@@ -11,6 +11,10 @@ Juego::Juego() {
     sprite->setTexture(*imagen);
     sprite->setScale(800.f / sprite->getTexture()->getSize().x, 600.f / sprite->getTexture()->getSize().y); // tamaño deseado dividido tamaño actual
 
+    Image img;
+    img.loadFromFile("caja.png");
+    ventana->setIcon(img.getSize().x, img.getSize().y, img.getPixelsPtr());
+
     font = new Font();
     font->loadFromFile("HU The Game.ttf");
 
@@ -21,10 +25,29 @@ Juego::Juego() {
     titulo.setPosition(400 - titulo.getGlobalBounds().width / 2.f, 50);
 
 
-    btnNuevoJuego = new Boton(400 - titulo.getGlobalBounds().width / 3.f, 180, 150, 80, &*font, "Nuevo Juego", 25, Color::Blue, Color::Green, Color::Red);
-    btnCargarJuego = new Boton(400 - titulo.getGlobalBounds().width / 3.f, 300, 150, 80, &*font, "Cargar Juego", 25, Color::Yellow, Color::Green, Color::Red);
-    btnSalir = new Boton(400 - titulo.getGlobalBounds().width / 3.f, 420, 150, 80, &*font, "Salir", 25, Color::Red, Color::Green, Color::Black);
+    btnNuevoJuego = new Boton(400 - titulo.getGlobalBounds().width / 3.f, 140, 170, 80, &*font, "Nuevo Juego", 25, Color::Blue, Color::Green, Color::Red);
+    btnCargarJuego = new Boton(400 - titulo.getGlobalBounds().width / 3.f, 260, 170, 80, &*font, "Cargar Juego", 25, Color::Yellow, Color::Green, Color::Red);
+    btnSolucionNivel = new Boton(400 - titulo.getGlobalBounds().width / 3.f, 380, 170, 80, &*font, "Solucion Nivel", 25, Color::Cyan, Color::Green, Color::Black);
+    btnSalir = new Boton(400 - titulo.getGlobalBounds().width / 3.f, 500, 170, 80, &*font, "Salir", 25, Color::Red, Color::Green, Color::Black);
 
+    this->estadoSubmenu = SBMN_INACTIVO;
+
+    descripcion.setFont(*font);
+    descripcion.setString("Selecione uno de los siguientes niveles");
+    descripcion.setCharacterSize(20);
+    descripcion.setFillColor(Color::White);
+    descripcion.setPosition(400 - titulo.getGlobalBounds().width, 80);
+
+
+    btnN1 = new Boton(140, 180, 80, 80, &*font, "n1", 25, Color::Blue, Color::Green, Color::Red);
+    btnN2 = new Boton(240, 180, 80, 80, &*font, "n2", 25, Color::Blue, Color::Green, Color::Red);
+    btnN3 = new Boton(340, 180, 80, 80, &*font, "n3", 25, Color::Blue, Color::Green, Color::Red);
+    btnN4 = new Boton(440, 180, 80, 80, &*font, "n4", 25, Color::Blue, Color::Green, Color::Red);
+    btnN5 = new Boton(540, 180, 80, 80, &*font, "n5", 25, Color::Blue, Color::Green, Color::Red);
+
+
+    btnSiguiente = new Boton(400 - titulo.getGlobalBounds().width / 3.f, 300, 150, 80, &*font, "Siguiente", 25, Color::Yellow, Color::Green, Color::Red);
+    btnAtras = new Boton(400 - titulo.getGlobalBounds().width / 3.f, 420, 150, 80, &*font, "Atras", 25, Color::Red, Color::Green, Color::Black);
 }
 
 void Juego::mostrarVentana() // Loop principal
@@ -62,9 +85,26 @@ void Juego::procesarEventos()
         // get mouse position
         Vector2f posMouse = sf::Vector2f(sf::Mouse::getPosition(*ventana));
 
-        btnNuevoJuego->actualizarBoton(posMouse);
-        btnCargarJuego->actualizarBoton(posMouse);
-        btnSalir->actualizarBoton(posMouse);
+        if (estadoSubmenu == SBMN_INACTIVO) {
+
+            btnNuevoJuego->actualizarBoton(posMouse);
+            btnCargarJuego->actualizarBoton(posMouse);
+            btnSolucionNivel->actualizarBoton(posMouse);
+            btnSalir->actualizarBoton(posMouse);
+        }
+
+
+        if (estadoSubmenu != SBMN_INACTIVO) {
+
+            btnN1->actualizarBoton(posMouse);
+            btnN2->actualizarBoton(posMouse);
+            btnN3->actualizarBoton(posMouse);
+            btnN4->actualizarBoton(posMouse);
+            btnN5->actualizarBoton(posMouse);
+
+            btnSiguiente->actualizarBoton(posMouse);
+            btnAtras->actualizarBoton(posMouse);
+        }
 
     }
 }
@@ -75,21 +115,87 @@ void Juego::graficar()
 
     ventana->draw(*sprite); //imagen
 
-    ventana->draw(titulo);
+    if (estadoSubmenu == SBMN_INACTIVO) {
 
-    if (btnNuevoJuego->procesarBoton(&*ventana)) { // Botones
-        cout << "Nuevo Juego" << endl;
-        btnNuevoJuego->setEstadoBoton(BTN_INACTIVO);
+        ventana->draw(titulo);
+
+        if (btnNuevoJuego->procesarBoton(&*ventana)) { // Botones
+            cout << "Nuevo Juego" << endl;
+            btnNuevoJuego->setEstadoBoton(BTN_INACTIVO);
+            estadoSubmenu = SBMN_NUEVOJUEGO;
+        }
+
+        if (btnCargarJuego->procesarBoton(&*ventana)) {
+            cout << "Cargar Juego" << endl;
+            btnCargarJuego->setEstadoBoton(BTN_INACTIVO);
+            estadoSubmenu = SBMN_CARGARJUEGO;
+        }
+
+        if (btnSolucionNivel->procesarBoton(&*ventana)) {
+            cout << "Solucion Nivel" << endl;
+            btnSolucionNivel->setEstadoBoton(BTN_INACTIVO);
+            estadoSubmenu = SBMN_SOLUCIONNIVEL;
+        }
+
+        if (btnSalir->procesarBoton(&*ventana)) {
+            cout << "Salir Juego" << endl;
+            btnSalir->setEstadoBoton(BTN_INACTIVO);
+            ventana->close();
+        }
+
     }
 
-    if (btnCargarJuego->procesarBoton(&*ventana)) {
-        cout << "Cargar Juego" << endl;
-        btnCargarJuego->setEstadoBoton(BTN_INACTIVO);
+    if (estadoSubmenu != SBMN_INACTIVO) {
+
+        if (btnN1->procesarBoton(&*ventana)) { // Botones
+            cout << "n1" << endl;
+            btnN1->setEstadoBoton(BTN_INACTIVO);
+        }
+
+        if (btnN2->procesarBoton(&*ventana)) { // Botones
+            cout << "n2" << endl;
+            btnN2->setEstadoBoton(BTN_INACTIVO);
+        }
+
+        if (btnN3->procesarBoton(&*ventana)) { // Botones
+            cout << "n3" << endl;
+            btnN3->setEstadoBoton(BTN_INACTIVO);
+        }
+
+        if (btnN4->procesarBoton(&*ventana)) { // Botones
+            cout << "n4" << endl;
+            btnN4->setEstadoBoton(BTN_INACTIVO);
+        }
+
+        if (btnN5->procesarBoton(&*ventana)) { // Botones
+            cout << "n5" << endl;
+            btnN5->setEstadoBoton(BTN_INACTIVO);
+        }
+
+        if (btnSiguiente->procesarBoton(&*ventana)) { // Botones
+            cout << "Siguiente" << endl;
+            btnSiguiente->setEstadoBoton(BTN_INACTIVO);
+        }
+
+        if (btnAtras->procesarBoton(&*ventana)) {
+            cout << "Atras" << endl;
+            btnAtras->setEstadoBoton(BTN_INACTIVO);
+            estadoSubmenu = SBMN_INACTIVO;
+        }
+
     }
 
-    if (btnSalir->procesarBoton(&*ventana)) {
-        cout << "Salir Juego" << endl;
-        btnSalir->setEstadoBoton(BTN_INACTIVO);
+    if (estadoSubmenu == SBMN_NUEVOJUEGO) {
+        descripcion.setString("Seleccione uno de los niveles, Para iniciar un Nuevo Juego");
+        ventana->draw(descripcion);
+    }
+    if (estadoSubmenu == SBMN_CARGARJUEGO) {
+        descripcion.setString("Seleccione uno de los niveles, Para Cargar un Juego Anterior");
+        ventana->draw(descripcion);
+    }
+    if (estadoSubmenu == SBMN_SOLUCIONNIVEL) {
+        descripcion.setString("Seleccione uno de los niveles, Para Visualizar su Solucion");
+        ventana->draw(descripcion);
     }
 
 }
--- Sokoban/Juego.h
+++ Sokoban/Juego.h
@@ -9,6 +9,8 @@
 using namespace sf;// este es para usar el SFML
 using namespace std;
 
+enum estados_submenu { SBMN_INACTIVO = 0, SBMN_NUEVOJUEGO, SBMN_CARGARJUEGO, SBMN_SOLUCIONNIVEL };
+
 class Juego {
 private:
 	Matriz mat;
@@ -20,7 +22,21 @@ private:
 
 	Boton* btnNuevoJuego;
 	Boton* btnCargarJuego;
+	Boton* btnSolucionNivel;
 	Boton* btnSalir;
+
+	short unsigned estadoSubmenu; // unsigned es para no aceptar negativos
+
+	Text descripcion;
+	Boton* btnSiguiente;
+	Boton* btnAtras;
+	Boton* btnN1;
+	Boton* btnN2;
+	Boton* btnN3;
+	Boton* btnN4;
+	Boton* btnN5;
+
+
 public:
 
 	Juego();
@@ -28,7 +44,6 @@ public:
 	void dibujarVentana();
 	void procesarEventos();
 	void graficar();
-
 
 };
 
